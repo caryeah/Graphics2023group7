78bd749b680513a26bdb240a0f4926ac0811f516
MRVitheproducer <kunenegasela26@gmail.com>
2023-10-27 09:36:13 +0200

    Add project files.

---
+++ vertex_shader.glsl
@@ -0,0 +1,16 @@
+#version 330 core
+
+
+layout(location = 1) in vec3 vertexColor;
+
+
+out vec3 fragmentColor;
+
+void main(){
+
+    [...]
+
+    // The color of each vertex will be interpolated
+    // to produce the color of each fragment
+    fragmentColor = vertexColor;
+}
